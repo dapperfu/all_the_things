2a1422265f0cc7d268c54f8cbc44e39d491f2d00
Jed <jed@8600k.local>
2018-09-09 21:19:59 -0400

    .

---
+++ all_compiler.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Build accellerators
+apt-get install --yes icecc ccache
+
+# Build accellerators.
+ICECC=`which icecc`
+CCACHE=`which ccache`
+
+ICECC_BIN=${HOME}/opt/icecc/bin
+CCACHE_BIN=${HOME}/opt/ccache/bin
+
+rm -rf ${ICECC_BIN}
+rm -rf ${CCACHE_BIN}
+
+mkdir -p ${ICECC_BIN} ${CCACHE_BIN}
+
+debs=
+# GCC
+for GCC_VER in 4.8 5 6 7 8
+do
+	debs+="gcc-${GCC_VER} g++-${GCC_VER} "
+	ln -s ${ICECC} ${ICECC_BIN}/gcc-${GCC_VER}
+	ln -s ${ICECC} ${ICECC_BIN}/g++-${GCC_VER}
+	ln -s ${CCACHE} ${CCACHE_BIN}/gcc-${GCC_VER}
+	ln -s ${CCACHE} ${CCACHE_BIN}/g++-${GCC_VER}
+done
+
+# LLVM
+for CLANG_VER in 3.9 4.0 5.0 6.0 7 8
+do
+	debs+="clang-${CLANG_VER} "
+	ln -s ${ICECC} ${ICECC_BIN}/clang-${CLANG_VER}
+	ln -s ${ICECC} ${ICECC_BIN}/clang++-${CLANG_VER}
+	ln -s ${CCACHE} ${CCACHE_BIN}/clang-${CLANG_VER}
+	ln -s ${CCACHE} ${CCACHE_BIN}/clang++-${CLANG_VER}
+done
+
+apt-get install --yes ${debs}
---
+++ all_py.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+VERS="2.4.6 2.5.6 2.6.9 2.7.15 3.0.1 3.1.5 3.2.6 3.3.7 3.4.9 3.5.6 3.6.6 3.7.0"
+
+export BIN_ROOT=${WORKSPACE:-${HOME}}/opt
+export PYTHON_ROOT=${BIN_ROOT}/python
+export JOBS=${JOBS:-8}
+
+echo ${BIN_ROOT}
+for VER in ${VERS}
+do
+export URL=https://www.python.org/ftp/python/${VER}/Python-${VER}.tgz
+curl -L ${URL} | tar xzv
+cd Python-${VER}
+./configure --prefix=${PYTHON_ROOT}
+make install -j ${JOBS}
+done
